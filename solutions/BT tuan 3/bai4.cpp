4a65b62e97fcaa3dffb1b7c9630919cb2ffcbd39
hungg_261 <107567770+hungg261@users.noreply.github.com>
2025-01-07 17:22:43 +0700

    Create bai4.cpp

---
+++ solutions/BT tuan 3/bai4.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+
+const int MAX=1000;
+int mark[MAX+5];
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    int n;
+    cin>>n;
+    
+    while(n--){
+        int cur;
+        cin>>cur;
+        
+        ++mark[cur];
+    }
+    
+    int target=*max_element(mark,mark+MAX+1);
+    for(int i=0;i<=MAX;++i){
+        if(mark[i]==target){
+            cout<<i<<' ';
+        }
+    }
+    return 0;
+}
