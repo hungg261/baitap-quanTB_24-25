95e98991df1c5330062f6a47d52274cf476ef875
hungg_261 <107567770+hungg261@users.noreply.github.com>
2025-02-04 20:00:35 +0700

    Create Gold.cpp

---
+++ solutions/Bai tap Chat nhi phan/Gold.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int MAXN=50000;
+int n,k,a[MAXN];
+
+bool check(int mid){
+    int cnt=0;
+    for(int i=0;i<n;){
+        i=upper_bound(a,a+n,a[i]+2*mid+1)-a;
+        ++cnt;
+    }
+    return cnt<=k;
+}
+
+int tknp(){
+    int l=0,h=1e9,res=-1;
+    while(l<=h){
+        int mid=(l+h)/2;
+        if(check(mid)){
+            h=mid-1;
+            res=mid;
+        }
+        else l=mid+1;
+    }
+    return res;
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin>>n>>k;
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+    }
+    sort(a,a+n);
+    
+    cout<<tknp()<<'\n';
+    return 0;
+}
