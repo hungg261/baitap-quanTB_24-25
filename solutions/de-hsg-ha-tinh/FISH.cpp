b4ba5b74530b2947ed5f7b144ccaa69316bf5757
hungg_261 <107567770+hungg261@users.noreply.github.com>
2025-02-05 18:36:29 +0700

    Update FISH.cpp

--- solutions/de-hsg-ha-tinh/FISH.cpp
+++ solutions/de-hsg-ha-tinh/FISH.cpp
@@ -1,25 +1,22 @@
 #include<bits/stdc++.h>
-#define int long long
 using namespace std;
 
 const int MAX=1e6;
 int a[MAX+1],n;
 
 void solve(){
-    int cur=a[1];
-    int ans=cur*3;
-    for(int i=2;i<=n;++i){
-        if(a[i-1]>a[i]){
-            cur=a[i];
-        }
-        
-        ans+=cur*3;
+    int cur=1e9;
+    long long ans=0;
+    for(int i=1;i<=n;++i){
+        cur=min(cur,a[i]);
+        ans+=cur*3LL;
     }
     
     cout<<ans<<'\n';
 }
 
 signed main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
     cin>>n;
     for(int i=1;i<=n;++i){
         cin>>a[i];
